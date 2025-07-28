d1428dcf1297160f0e4081be6d8c606a1fa382c7
AngeloCassano <angel96ka@gmail.com>
2025-07-28 12:18:19 +0200

    addes classi Service di TierList e TierListElement

---
+++ TierList/src/main/java/com/example/TierList/service/TierListService.java
@@ -0,0 +1,40 @@
+package com.example.TierList.service;
+
+import com.example.TierList.model.TierListElement;
+import com.example.TierList.repository.TierListRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+@RequiredArgsConstructor
+public class TierListService {
+
+private final TierListRepository tierListRepository;
+
+public List<TierList> findAll() {
+return tierListRepository.findAll();
+}
+
+public List<TierListRepository> findByUtenteId(Long utenteId) {
+return tierListRepository.findByUtenteId(utenteId);
+}
+
+public List<TierListRepository> findByCategoryId(Long CategoryId) {
+return tierListRepository.findByCatgoryId(CategoryId);
+}
+
+public TierList findById(Long id) {
+return tierListrepository.findById(id).orElseThrow(() -> new RuntimeException("Tierlist non trovata"));
+}
+
+public TierList save(TierList tierList) {
+return tierListRepository.save(tierList);
+}
+
+public void delete(Long id) {
+tierListRepository.deleteById(id);
+}
+}
