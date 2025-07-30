bc3fee763836809898b331a027ba55f2a8a3f5d0
GiovanniDadone <143901741+GiovanniDadone@users.noreply.github.com>
2025-07-30 14:35:17 +0200

    fix frontend funzionale da vecchio branch

--- TierList/src/main/webapp/tier-list/src/App.jsx
+++ TierList/src/main/webapp/tier-list/src/App.jsx
@@ -1,12 +1,23 @@
-import './App.css'
+import React from 'react';
+import TierProvider from './providers/TierProvider';
+import ElementRooster from './components/ElementRooster';
+import TierList from './components/TierList';
+import './App.css';
 
-function App() {
-
+const App = () => {
   return (
-    <>
-    <p>Hello Tier-List</p>       
-    </>
-  )
-}
+    <TierProvider>
+      <div className="app-container">
+        <h1 className="app-title">Tier List System</h1>
+        
+        {/* Element Rooster */}
+        <ElementRooster />
+        
+        {/* Tier List */}
+        <TierList />
+      </div>
+    </TierProvider>
+  );
+};
 
-export default App
+export default App;
